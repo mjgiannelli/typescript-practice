6f3e6d59ad0330406139df734778c7a6e7dbb52b
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-12-01 18:58:00 -0600

    interface practice

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,27 @@ const getFullName = (name: string, surname: string): string => {
 }
 
 console.log(getFullName('Mark', 'Giannelli'))
+
+// OBJECTS
+  // interfaces - create objects with specific properties
+    // put '?' after key to specify that the property is not mandatory
+    // need to separate name of interfaces and classes from each other
+        // done by adding 'Interface' after the name of the interface
+
+    interface UserInterface {
+    name: string;
+    age?: number;
+    getMessage?(): string;
+}
+
+const user: UserInterface = {
+    name: 'Mark',
+    age: 30,
+    getMessage() {
+        return 'Hello ' + name;
+    }
+}
+
+const user2: UserInterface = {
+    name: 'Jack',
+}
