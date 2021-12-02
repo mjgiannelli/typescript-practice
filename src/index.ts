5f3cf85cd39484cd58d5af98d5158e538c046aeb
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-12-01 18:25:58 -0600

    practice with variables and functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-const hello = 'world';
+// using 'const' will not let you change the value of a variable
+// using 'let' will let you change the value of the variable but only to the same type of the original variable
+
+let hello = 'world';
+
+// FUNCTIONS
+
+// have to specify the type of the variable being passed
+const getFullName = (name: string, surname: string): string => {
+    return name + ' ' + surname;
+}
+
+console.log(getFullName('Mark', 'Giannelli'))
